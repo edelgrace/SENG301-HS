e2c61534d9ad33c311f4c4cf2923f44e1af46993
Edel Altares <edel.altares@gmail.com>
2015-03-25 00:53:04 -0600

    Added test stubs

---
+++ Project/test/ControllerTest.java
@@ -0,0 +1,91 @@
+import Deck.*;
+import Cards.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+    
+    /* DISPLAYING CARD COLLECTION */
+    
+    @Test
+    public void Test_DisplayNullList() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayListWithNull() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayNoCards() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayCard() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayRow() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayPage() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayTwoPages() {
+        
+    }
+    
+    @Test
+    public void Test_Display1Card() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayAllCards() {
+        
+    }
+    
+    /* DISPLAY DECK */
+    
+    @Test
+    public void Test_DisplayEmptyDeck() {
+        
+    }
+    
+    @Test
+    public void Test_DisplayNullDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Display1CardDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Display2CardDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Display29CardDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Display30CardDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Display31CardDeck() {
+        
+    }
+}
---
+++ Project/test/SaveLoadTest.java
@@ -0,0 +1,89 @@
+import Deck.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SaveLoadTest {
+    
+    /* SAVING */
+    
+    @Test
+    public void Test_SaveTimestamp() {
+        
+    }
+    
+    @Test
+    public void Test_SaveEmptyDeck() {
+        
+    }
+    
+    @Test
+    public void Test_SaveOneCard() {
+        
+    }
+    
+    @Test
+    public void Test_SaveTwoCards() {
+        
+    }
+    
+    @Test
+    public void Test_SaveFullDeck() {
+        
+    }
+    
+    @Test
+    public void Test_Save31Cards() {
+        
+    }
+    
+    @Test
+    public void Test_Save29Cards() {
+        
+    }
+    
+    /* LOADING */
+    
+    @Test
+    public void Test_HandleFailLoad() {
+        
+    }
+    
+    @Test
+    public void Test_HandleNullLoad() {
+        
+    }
+    
+    @Test
+    public void Test_LoadTimestamp() {
+        
+    }
+    @Test
+    public void Test_LoadEmptyDeck() {
+        
+    }
+    
+    @Test
+    public void Test_LoadOneCard() {
+        
+    }
+    
+    @Test
+    public void Test_LoadTwoCards() {
+        
+    }
+    
+    @Test
+    public void Test_FullDeck() {
+        
+    }
+    
+    @Test
+    public void Test_29Cards() {
+        
+    }
+    
+    @Test
+    public void Test_31Cards() {
+        
+    }
+}
